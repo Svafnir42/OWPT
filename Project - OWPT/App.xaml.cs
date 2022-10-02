2c7902e0aac220bc71f3ce8c2215629aaec41ff1
Svafnir42 <hivay.zol@yandex.ru>
2022-10-02 21:55:19 +0400

    create maui project

---
+++ Project - OWPT/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Project___OWPT;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Project - OWPT/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Project___OWPT;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
